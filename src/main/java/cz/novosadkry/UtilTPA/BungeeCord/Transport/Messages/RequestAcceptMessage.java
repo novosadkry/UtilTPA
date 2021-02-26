da873437ede96e8fa72b248baceab42d832351e1
Kryštof Novosad <novosadkry@seznam.cz>
2021-02-26 15:16:55 +0100

    Implement RequestAcceptMessage class

--- src/main/java/cz/novosadkry/UtilTPA/BungeeCord/Transport/Messages/RequestAcceptMessage.java
+++ src/main/java/cz/novosadkry/UtilTPA/BungeeCord/Transport/Messages/RequestAcceptMessage.java
@@ -1,7 +1,13 @@
 package cz.novosadkry.UtilTPA.BungeeCord.Transport.Messages;
 
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import cz.novosadkry.UtilTPA.BungeeCord.Transport.Abstract.Message;
 import cz.novosadkry.UtilTPA.BungeeCord.Transport.Abstract.MessageType;
+import cz.novosadkry.UtilTPA.Main;
 import cz.novosadkry.UtilTPA.Request.Request;
+import org.bukkit.Bukkit;
 
 public class RequestAcceptMessage extends RequestMessage {
     public RequestAcceptMessage(Request request) {
@@ -15,6 +21,32 @@ public class RequestAcceptMessage extends RequestMessage {
 
     @Override
     public void send() {
+        ByteArrayDataOutput header = ByteStreams.newDataOutput();
 
+        // Write ForwardToPlayer header
+        header.writeUTF("ForwardToPlayer");
+        header.writeUTF(request.getFrom().getName());
+        header.writeUTF("UtilTPA");
+
+        ByteArrayDataOutput body = ByteStreams.newDataOutput();
+
+        // Write message data
+        body.writeShort(getType().ordinal());
+        body.writeUTF(request.getFrom().getName());
+        body.writeUTF(request.getTo().getName());
+
+        // Append message to header
+        byte[] bodyBytes = body.toByteArray();
+        header.writeShort(bodyBytes.length);
+        header.write(bodyBytes);
+
+        Bukkit.getServer().sendPluginMessage(Main.getInstance(), "BungeeCord", header.toByteArray());
+    }
+
+    public static Message resolve(ByteArrayDataInput data) {
+        String from = data.readUTF();
+        String to = data.readUTF();
+
+        return new RequestAcceptMessage(new Request(from, to));
     }
 }
