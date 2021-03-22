ca15854b560dda1eb9cf5b54b6bfd53ed4ceb787
Kryštof Novosad <novosadkry@seznam.cz>
2021-03-22 13:18:31 +0100

    Remove unnecessary method override

--- src/main/java/cz/novosadkry/UtilTPA/BungeeCord/Drivers/BungeeDriver.java
+++ src/main/java/cz/novosadkry/UtilTPA/BungeeCord/Drivers/BungeeDriver.java
@@ -4,7 +4,6 @@ import cz.novosadkry.UtilTPA.BungeeCord.Transport.Messages.Message;
 import cz.novosadkry.UtilTPA.BungeeCord.Transport.Messages.MessageEventHandler;
 import cz.novosadkry.UtilTPA.Main;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 
 public abstract class BungeeDriver extends MessageEventHandler implements IBungeeDriver {
 
@@ -17,9 +16,6 @@ public abstract class BungeeDriver extends MessageEventHandler implements IBunge
     public void sendMessage(Message msg) {
         Bukkit.getServer().sendPluginMessage(Main.getInstance(), "BungeeCord", msg.toBytes());
     }
-
-    @Override
-    abstract public void onPluginMessageReceived(String channel, Player player, byte[] bytes);
 
     public void terminate() {
         unregisterListeners();
